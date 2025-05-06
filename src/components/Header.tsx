336853cd4d23be2ab5e0e241d4608b138f568e44
hoxey2Front <159544052+hoxey2Front@users.noreply.github.com>
2025-05-07 01:52:17 +0900

    Header.tsx 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const Header = () => {
   return (
-    <nav className="fixed top-0 max-w-[1000px] w-full px-4 mx-auto mt-4 z-50">
+    <nav className="fixed top-0 max-w-[1000px] w-full px-4 mx-auto mt-4 bg-white dark:bg-black z-50">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-black text-white dark:text-[#eee]/70">
           <Link href="/" className="bg-[#896aff] hover:bg-[#6d45fc] hover:shadow-sm shadow-[#6d45fc] transition-all duration-300 ease-in-out px-4 py-2 rounded-sm">
